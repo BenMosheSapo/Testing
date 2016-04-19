5589308a903ad6aab5d3adb1253ad6c846ad8c07
Ben Sapo <bensapo@gmail.com>
2016-04-19 14:29:37 +0200

    Test

---
+++ FirstTest/Header.h
@@ -0,0 +1,30 @@
+#include "cxxtest\TestSuite.h"
+#include "../MyKey/MyKey.h"
+class MytestSuite : public CxxTest::TestSuite{
+public:
+	void testOut(void){
+		MyKey testkey(3, 4);
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
